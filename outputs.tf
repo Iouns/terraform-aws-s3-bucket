cea4912cb9d5557536181fea2ac93d94e2416756
Iouns <me@iouns.me>
2020-09-18 18:32:58 +0200

    Update to context.

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "bucket_domain_name" {
-  value       = var.enabled ? join("", aws_s3_bucket.default.*.bucket_domain_name) : ""
+  value       = module.this.enabled ? join("", aws_s3_bucket.default.*.bucket_domain_name) : ""
   description = "FQDN of bucket"
 }
 
 output "bucket_regional_domain_name" {
-  value       = var.enabled ? join("", aws_s3_bucket.default.*.bucket_regional_domain_name) : ""
+  value       = module.this.enabled ? join("", aws_s3_bucket.default.*.bucket_regional_domain_name) : ""
   description = "The bucket region-specific domain name"
 }
 
 output "bucket_id" {
-  value       = var.enabled ? join("", aws_s3_bucket.default.*.id) : ""
+  value       = module.this.enabled ? join("", aws_s3_bucket.default.*.id) : ""
   description = "Bucket Name (aka ID)"
 }
 
 output "bucket_arn" {
-  value       = var.enabled ? join("", aws_s3_bucket.default.*.arn) : ""
+  value       = module.this.enabled ? join("", aws_s3_bucket.default.*.arn) : ""
   description = "Bucket ARN"
 }
 
 output "enabled" {
-  value       = var.enabled
+  value       = module.this.enabled
   description = "Is module enabled"
 }
 
